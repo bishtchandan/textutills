a89aaa71cc3cfe3d46eec2f2bdd6a8e6d46be88e
Chandan Singh Bisht <chandanbisht085@gmail.com>
2022-01-11 15:18:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      document.title='TextModifier - Dark';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title='TextModifier - Light';
+    }
+  }
+
+  return (
+    <>
+    <Router>
+      {/* <Navbar title="TextUtils" aboutTxt="About"/> */}
+      <Navbar title="TextModifiers" aboutTxt="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      <div className="container my-4">
+      <Switch>
+          <Route exact path="/about">
+            <About mode={mode}/>
+          </Route>
+          <Route exact path="/">
+          <TextForm showAlert={showAlert} heading="Use TextModifier - word counting , character counting, & remove extra spaces" mode={mode} />
+          </Route>
+        </Switch>
+      </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
